beb15bdbe593b2323562822fb4e6c550a7a349aa
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-04 21:35:42 -0300

    Hero component

--- components/home-page/Hero.js
+++ components/home-page/Hero.js
@@ -1,3 +1,21 @@
+import Image from 'next/image';
+import classes from './hero.module.css';
 export const Hero = () => {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/unknown.jpeg'
+          alt='An Image showing Matias'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Matias</h1>
+      <p>
+        I blog about web and mobile developtment. Like React and React Native,
+        and something more.
+      </p>
+    </section>
+  );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head';
-
+import { Hero } from '../components/home-page/Hero';
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
