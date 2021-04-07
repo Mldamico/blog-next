19692a99d1ba10f430b1295d268072c91611e138
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-07 18:57:47 -0300

    storing in db

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
     if (
@@ -17,11 +19,32 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        process.env.DB_HOST
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connecto to database' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed' });
+      return;
+    }
+
+    client.close();
     res
       .status(201)
       .json({ message: 'Successfully stored message!', message: newMessage });
   }
 }
-
+//HZQ5pM9c6NXbO2VH
 export default handler;
