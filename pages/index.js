7c43873bafa82c0ffb1baaa1ff0a22669091bb91
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-07 17:16:01 -0300

    getStaticProps featuredPosts and allPosts

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,23 @@
 import Head from 'next/head';
 import { Hero } from '../components/home-page/Hero';
 import { FeaturedPosts } from '../components/home-page/FeaturedPosts';
+import { getFeaturedPosts } from '../lib/posts-util';
 
-export const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a React Framework for production. It makes building fullstack reacts app and sited a breeze and ships with built-in SSR.',
-    date: '2021-04-06',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs2.png',
-    excerpt:
-      'NextJS is a React Framework for production. It makes building fullstack reacts app and sited a breeze and ships with built-in SSR.',
-    date: '2021-04-06',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs3.png',
-    excerpt:
-      'NextJS is a React Framework for production. It makes building fullstack reacts app and sited a breeze and ships with built-in SSR.',
-    date: '2021-04-06',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs4.png',
-    excerpt:
-      'NextJS is a React Framework for production. It makes building fullstack reacts app and sited a breeze and ships with built-in SSR.',
-    date: '2021-04-06',
-  },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head';
 import { AllPosts } from '../../components/posts/AllPosts';
-import { DUMMY_POSTS } from '../index';
+import { getAllPosts } from '../../lib/posts-util';
 
-export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export default function AllPostsPage({ posts }) {
+  return <AllPosts posts={posts} />;
+}
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
