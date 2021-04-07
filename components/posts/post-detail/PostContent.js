32b1bfbb760922ec6c00d99becf80059d808a5bc
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-07 18:24:56 -0300

    Image in markdown

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,13 +1,35 @@
 import classes from './PostContent.module.css';
 import { PostHeader } from './PostHeader';
 import ReactMarkdown from 'react-markdown';
-
+import Image from 'next/image';
 export const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
