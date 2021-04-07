27dfe25529dabca5a73f686b811bec7c6f4b0bdf
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-07 17:50:04 -0300

    Single post

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,19 +1,13 @@
 import classes from './PostContent.module.css';
 import { PostHeader } from './PostHeader';
 import ReactMarkdown from 'react-markdown';
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started With NextJS',
-  image: 'getting-started-nextjs.png',
-  date: '2021-04-06',
-  content: '# This is a first post',
-};
-export const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+export const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,28 @@
 import Head from 'next/head';
 import { PostContent } from '../../components/posts/post-detail/PostContent';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-export default function SinglePostPage() {
-  return <PostContent />;
+export default function SinglePostPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps({ params }) {
+  const { slug } = params;
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((filename) => filename.replace(/\.md$/, ''));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
