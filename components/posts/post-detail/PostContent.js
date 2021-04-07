a226611bd77f2e16827db3664f4cfbe9fb181041
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-07 18:32:54 -0300

    Code snippets

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,6 +2,8 @@ import classes from './PostContent.module.css';
 import { PostHeader } from './PostHeader';
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 export const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
@@ -23,6 +25,15 @@ export const PostContent = ({ post }) => {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+    code({ language, value }) {
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
     },
   };
 
